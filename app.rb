80a44124a54c85a715bbe41921c86b48749bcede
Tom Dooner <tomdooner@gmail.com>
2011-12-31 20:00:12 -0500

    Add a lot of interface things.

--- app.rb
+++ app.rb
@@ -1,18 +1,29 @@
 require 'steam-condenser' 
 require 'sinatra'
+require 'json'
+
+configure do
+    enable :sessions
+    set :public_folder, File.dirname(File.expand_path(__FILE__)) + "/public"
+end
 
 get '/' do 
-    haml :index
+    haml :first if not session[:steam_id]
 end
 
 post '/' do
     a = SteamId.new(params[:steam_id])
     a.fetch_games
-    str = haml :index 
-    str << "<h1>What #{params[:steam_id]} owns:</h1>"
-    a.games.each do |k,e|
-        str << e.name + "<br />"
-    end
+    a.fetch
+    @username = a.nickname
+    @games = a.games
+    @friends = a.friends
 
-    str 
+    haml :gamelist
+end
+
+get '/ajax/:id' do |id|
+    a = SteamId.new(id.to_i, true)
+    
+    {:nickname=>a.nickname, :icon_url=>a.icon_url}.to_json
 end
